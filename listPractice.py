f0431fa0656516afc0962971eb133ab5bfe1d833
surya1243 <surya1243@outlook.com>
2022-03-07 16:35:18 +0545

    list practice in python

---
+++ listPractice.py
@@ -0,0 +1,15 @@
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+print(myList[0])
+print(myList[1])
+print(myList[2])
+
+#printing all the list elements
+print("printing all the list elements of myList")
+for x in myList:
+    print(x)
+
+print("The second element of the list is %d" % myList[1])
+print("The first element of the list is", myList[0])
