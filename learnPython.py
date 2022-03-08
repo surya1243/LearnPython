f85b36fc8eece9b4e672680aa87a689cbe94737c
surya1243 <surya1243@outlook.com>
2022-03-08 12:01:39 +0545

    Python operator precendence

--- learnPython.py
+++ learnPython.py
@@ -18,3 +18,4 @@ if isinstance(myfloat, float) and myfloat == 10.0:
     print("Float: %f" % myfloat)
 if isinstance(myint, int) and myint == 20:
     print("Integer: %d" % myint)
+    
---
+++ operators.py
@@ -0,0 +1,10 @@
+#precedence hiararchy of operator
+print(2+2*7/7)
+
+#most of Python's operators have left-sided binding
+print(9 % 6 % 2)
+
+#but except for exponentiation, the exponentiation operator uses right-sided binding
+print(2 ** 2 ** 3)
+
+print((-2 / 4), (8 / 4), (2 // 4), (-2 // 4))
